4ab8402fa9bd9374af46697065fd6ca2c8d317f4
underway <underwayu@gmail.com>
2022-07-03 19:58:57 +0200

    Add facilities for creating dependency graph for patches
    PIXI, GPS, AMK and UberASM will be more annoying, because I will have to parse their list files and command line options but should also be workable at some point.

--- DependencyGraph.cs
+++ DependencyGraph.cs
@@ -7,7 +7,7 @@ using System.Text.RegularExpressions;
 using System.IO;
 
 using QuickGraph.Algorithms;
-using QuickGraph.Collections;
+using QuickGraph;
 
 using SMWPatcher;
 
@@ -15,29 +15,81 @@ namespace LunarHelper
 {
     class DependencyGraph
     {
-        private static readonly Regex include_regex = new Regex("^\\s*(incsrc|incbin)\\s+(\"(.*)\"|[^\\s]*)",
-            RegexOptions.IgnoreCase | RegexOptions.Compiled);
-
-        private static HashSet<string> GetIncludes(string file_path)
+        private class Vertex
         {
-            HashSet<string> includes = new HashSet<string>();
+            public string absolute_file_path;
+            public string hash;
 
-            using (StreamReader sr = new StreamReader(file_path))
+            public Vertex(string absolute_file_path)
             {
-                string line;
+                this.absolute_file_path = absolute_file_path;
+                hash = Report.HashFile(absolute_file_path);
+            }
+        }
 
-                while ((line = sr.ReadLine()) != null)
+        private AdjacencyGraph<Vertex, SEdge<Vertex>> dependency_graph;
+        private Dictionary<string, Vertex> file_vertex_dict = new Dictionary<string, Vertex>();
+
+        private HashSet<Vertex> pixi_sources;
+        private HashSet<Vertex> uberasm_sources;
+        private HashSet<Vertex> gps_sources;
+        private HashSet<Vertex> amk_sources;
+        private HashSet<Vertex> patch_sources;
+
+        public DependencyGraph(ICollection<string> pixi_sources, ICollection<string> uberasm_sources, ICollection<string> gps_sources,
+            ICollection<string> amk_sources, ICollection<string> patch_list_sources, ICollection<string> patch_folder_sources)
+        {
+            dependency_graph = new AdjacencyGraph<Vertex, SEdge<Vertex>>();
+
+            // this.pixi_sources = CreateSourceSet(pixi_sources);
+            // this.uberasm_sources = CreateSourceSet(uberasm_sources);
+            // this.gps_sources = CreateSourceSet(gps_sources);
+            // this.amk_sources = CreateSourceSet(amk_sources);
+            this.patch_sources = CreateSourceSet(patch_list_sources);
+
+
+        }
+
+        private void AddPatchListVertexDependencies(Vertex vertex)
+        {
+            HashSet<Vertex> visited = new HashSet<Vertex>();
+
+            foreach (var dependency in DependencyResolver.GetAsarFileDependencies(vertex.absolute_file_path))
+            {
+                visited.Add(vertex);
+                Vertex dependency_vertex = GetOrCreateVertex(dependency.absolute_dependency_path);
+                dependency_graph.AddEdge(new SEdge<Vertex>(vertex, dependency_vertex));
+                if (!visited.Contains(dependency_vertex) && !dependency.is_deadend)
                 {
-                    Match match = include_regex.Match(line);
-
-                    if (match.Success)
-                    {
-                        includes.Add(match.Groups[2].Value.Trim('"'));
-                    }
+                    AddPatchListVertexDependencies(dependency_vertex);
                 }
             }
+        }
 
-            return includes;
+        private HashSet<Vertex> CreateSourceSet(ICollection<string> sources)
+        {
+            HashSet<Vertex> source_set = new HashSet<Vertex>();
+            foreach (string source in sources)
+            {
+                source_set.Add(GetOrCreateVertex(source));
+            }
+            return source_set;
+        }
+
+        private Vertex GetOrCreateVertex(string absolute_file_path)
+        {
+            Vertex existing_vertex;
+            file_vertex_dict.TryGetValue(absolute_file_path, out existing_vertex);
+
+            if (existing_vertex != null)
+            {
+                return existing_vertex;
+            }
+            
+            Vertex vertex = new Vertex(absolute_file_path);
+            dependency_graph.AddVertex(vertex);
+            file_vertex_dict.Add(vertex.absolute_file_path, vertex);
+            return vertex;
         }
     }
 }
---
+++ DependencyResolver.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace LunarHelper
+{
+    class DependencyResolver
+    {
+        public class Dependency
+        {
+            public string absolute_dependency_path;
+            // true if the absolute_dependency_path points to a file that should not be scanned for further dependencies
+            // i.e. if it was included via incbin
+            public bool is_deadend;
+
+            public Dependency(string absolute_dependency_path, bool is_deadend)
+            {
+                this.absolute_dependency_path = absolute_dependency_path;
+                this.is_deadend = is_deadend;
+            }
+        }
+
+        private static readonly Regex incsrc_incbin_regex = new Regex("^\\s*(incsrc|incbin)\\s+(\"(.*)\"|[^\\s]*)",
+            RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+        public static HashSet<Dependency> GetAmkListDependencies(string absolute_amk_list_path)
+        {
+            HashSet<Dependency> dependencies = new HashSet<Dependency>();
+
+            // TODO
+
+            return dependencies;
+        }
+
+        public static HashSet<Dependency> GetUberAsmListDependencies(string absolute_uberasm_list_path)
+        {
+            HashSet<Dependency> dependencies = new HashSet<Dependency>();
+
+            // TODO
+
+            return dependencies;
+        }
+
+        public static HashSet<Dependency> GetGpsListDependencies(string absolute_gps_list_path)
+        {
+            HashSet<Dependency> dependencies = new HashSet<Dependency>();
+
+            // TODO
+
+            return dependencies;
+        }
+
+        public static HashSet<Dependency> GetPixiListDependencies(string absolute_pixi_list_path)
+        {
+            HashSet<Dependency> dependencies = new HashSet<Dependency>();
+
+            // TODO
+
+            return dependencies;
+        }
+
+        public static HashSet<Dependency> GetPixiFileDependencies(string absolute_pixi_file_path)
+        {
+            switch (Path.GetExtension(absolute_pixi_file_path))
+            {
+                case ".asm": return GetAsarFileDependencies(absolute_pixi_file_path);
+                case ".json": return GetPixiCfgFileDependencies(absolute_pixi_file_path);
+                case ".cfg": return GetPixiJsonFileDependencies(absolute_pixi_file_path);
+                default: return new HashSet<Dependency>();
+            }
+        }
+
+        private static HashSet<Dependency> GetPixiJsonFileDependencies(string absolute_json_path)
+        {
+            HashSet<Dependency> dependencies = new HashSet<Dependency>();
+
+            try
+            {
+                string contents = File.ReadAllText(absolute_json_path);
+                JsonNode node = JsonNode.Parse(contents);
+                // FIXME Handle different sprite, generator, shooter, etc. folders and resolve "AsmFile" path 
+                // to an actual full path
+                dependencies.Add(new Dependency(node["AsmFile"].ToString(), false));
+            }
+            catch (Exception)
+            {
+                // pass
+            }
+
+            return dependencies;
+        }
+
+        private static HashSet<Dependency> GetPixiCfgFileDependencies(string absolute_cfg_path)
+        {
+            HashSet<Dependency> dependencies = new HashSet<Dependency>();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(absolute_cfg_path))
+                {
+                    int idx = 0;
+
+                    while (++idx != 4)
+                    {
+                        sr.ReadLine();
+                    }
+
+                    // FIXME Handle different sprite, generator, shooter, etc. folders and resolve "AsmFile" path 
+                    // to an actual full path
+                    dependencies.Add(new Dependency(sr.ReadLine(), false));
+                }
+            }
+            catch (Exception)
+            {
+                // pass
+            }
+
+            return dependencies;
+        }
+
+        public static HashSet<Dependency> GetAsarFileDependencies(string absolute_asar_path)
+        {
+            HashSet<Dependency> dependencies = new HashSet<Dependency>();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(absolute_asar_path))
+                {
+                    string line;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Match match = incsrc_incbin_regex.Match(line);
+
+                        if (match.Success)
+                        {
+                            string trimmed_include_path = match.Groups[2].Value.Trim('"');
+                            if (!Path.IsPathRooted(trimmed_include_path))
+                            {
+                                trimmed_include_path = Path.GetFullPath(
+                                    Path.Combine(Path.GetDirectoryName(absolute_asar_path), trimmed_include_path));
+                            }
+
+                            // if the include is an incbin, mark this dependency as a deadend
+                            dependencies.Add(new Dependency(trimmed_include_path, match.Groups[1].Value.ToLower() == "incbin"));
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // pass
+            }
+
+            return dependencies;
+        }
+    }
+}
